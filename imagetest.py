c46f76eb48fdaad20eddbebfee712fb23e7fc625
Nathan Smith <smithn4@middleton.school.nz>
2023-05-15 17:03:23 +1200

    Finished first component v1. Before trialling

--- imagetest.py
+++ imagetest.py
@@ -1,10 +1,22 @@
 import tkinter as tk
-from tkinter import ttk
-from PIL import ImageTk, Image
-
 
 root = tk.Tk()
-img1 = ImageTk.PhotoImage(Image.open("multi-color-rainbow-background.JPG"))
-ttk.Button(root, text="Click", image=img1).grid(row=0, column=0)
 
-tk.mainloop()
+# create a canvas and add a background image
+canvas = tk.Canvas(root, width=400, height=400)
+background_image = tk.PhotoImage(file="Dalle-Background.PNG")
+canvas.create_image(0, 0, anchor='nw', image=background_image)
+canvas.pack()
+
+# create a transparent frame
+transparent_frame = tk.Frame(root, width=200, height=200, bg='white')
+transparent_frame.place(relx=0.5, rely=0.5, anchor='center')
+
+# create a transparent canvas to overlay on the frame
+overlay_canvas = tk.Canvas(transparent_frame, width=200, height=200)
+overlay_canvas.place(x=0, y=0)
+
+# set the transparent color of the overlay canvas to white
+overlay_canvas.config(highlightthickness=0, bd=0, **{"transparentcolor":'white'})
+
+root.mainloop()
---
+++ initialise_GUIv1.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+from tkinter import ttk
+from PIL import ImageTk, Image
+import ast
+
+
+class Root(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        #Define styles
+        s = ttk.Style()
+        s.configure("TFrame", background="#bf3b3b")
+        self.geometry("800x800")
+        self.resizable(False, False)
+        self.title("Te Reo Quiz")
+        #Background image, using a label to display it
+        self.bgimg = ImageTk.PhotoImage(Image.open("Dalle-Background.PNG"))
+        self.bglabel = ttk.Label(self, i=self.bgimg)
+        self.bglabel.pack()
+        #Main frame of GUI
+        self.mainframe = ttk.Frame(self.bglabel)
+        self.mainframe.grid(row=0, column=0, padx=250, pady=250)
+        #Label for testing
+        ttk.Label(self.mainframe, text="test", background="#bf3b3b").grid(row=0, column=0, padx=150, pady=150)
+        #Import words from file
+        self.getdict()
+        for word_type in self.words_dict:
+            print(f"\n{word_type}: ")
+            for term in self.words_dict[word_type]:
+                print(f"{term}: {self.words_dict[word_type][term]}")
+
+
+    #Function that imports maori words and definitions from file
+    def getdict(self):
+        with open("maori_words.txt", "r") as file:
+            self.words_dict = ast.literal_eval(file.read())
+
+
+if __name__ == "__main__":
+    root = Root()
+    tk.mainloop()
